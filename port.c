3331618e8c94f962076a119187d1e91688ae9fd2
Anosh D. Ullenius <anosh@anosh.se>
2023-04-10 21:32:33 +0200

    Replace non-standard byte-literals. Replace short with exact type

--- port.c
+++ port.c
@@ -8,49 +8,48 @@
 
 // beaten = 0, alive = 1
 uint32_t bubbleman(uint32_t alive) {
-    return (alive) ? 0b10000000 : 0b10000000000; // C3 / D1
+    return alive ? 1 << 7 : 1 << 10; // C3 / D1
 }
 
 uint32_t airman(uint32_t alive) {
-    return (alive) ? 0b100000000000 : 0b100000000000000000; // D2 / E3
+    return alive ? 1 << 11 : 1 << 17; // D2 / E3
 }
 
 uint32_t quickman(uint32_t alive) {
-    return (alive) ? 0b100000000 : 0b1000; // C4 / B4
+    return alive ? 1 << 8 : 1 << 3; // C4 / B4
 }
 
 uint32_t woodman(uint32_t alive) {
-    return (alive) ? 0b10000 : 0b1000000000000; // B5 / D3
+    return alive ? 1 << 4 : 1 << 12; // B5 / D3
 }
 
 uint32_t crashman(uint32_t alive) {
-    return (alive) ? 0b10000000000000000 : 0b1000000000; // E2 / C5
+    return alive ? 1 << 16 : 1 << 9; // E2 / C5
 }
 
 uint32_t flashman(uint32_t alive) {
-    return (alive) ? 0b01000000000000000000 : 0b100000; // E4 / C1
+    return alive ? 1 << 18: 1 << 5; // E4 / C1
 }
 
 uint32_t metalman(uint32_t alive) {
-    return (alive) ? 0b1000000000000000 : 0b10000000000000000000; // E1 / E5
+    return alive ? 1 << 15: 1 << 19; // E1 / E5
 }
 uint32_t heatman(uint32_t alive) {
-    return (alive) ? 0b100000000000000 : 0b10; //  D5 / B2
+    return alive ? 1 << 14 : 1 << 1; //  D5 / B2
 }
     
 uint32_t etanks(const  char amount) {
 
-     uint32_t etanks = (1 << (WORD_SIZE + amount));
+    uint32_t etanks = (1 << (WORD_SIZE + amount));
     return etanks;
 }
 
-uint32_t bitSet(const  uint32_t bits, const  short pos) {
+uint32_t bitSet(const uint32_t bits, const uint8_t pos) {
 
     return ( (bits & (1 << (pos - 1))) != 0);
 }
 
-void decodePassword(const  uint32_t bits, const char letter, const short OFFSET) {
-
+void decodePassword(const uint32_t bits, const char letter, const uint8_t OFFSET) {
     for (uint32_t i = 0; i < 5; i++) {
         if (bitSet(bits, (OFFSET + i ))) {
             printf("%c%i ", letter, (i + 1));
@@ -72,32 +71,32 @@ void decode(const  uint32_t bits) {
 
     Call this function *before* etanks()
 */
- uint32_t rotateLeft(const  uint32_t bits, const  short ETANKS) {
+ uint32_t rotateLeft(const  uint32_t bits, const uint8_t ETANKS) {
 
     assert(ETANKS <= MAX_ETANKS);
     if (ETANKS == 0) {
         return bits;
     }
-    const  uint32_t mask = 0xFFFFF; // bits 1-20
+    const uint32_t mask = 0xFFFFF; // bits 1-20
 
     // clear first 5 bits used for etanks (A word)
-     uint32_t leftmost = bits & mask;
+    uint32_t leftmost = bits & mask;
     // left rotation of bits by 1-4 steps. 
     // 20 bit word size
     leftmost = (bits >> (WORD_SIZE - ETANKS));
 
-     uint32_t rightmost = (bits << ETANKS);
+    uint32_t rightmost = (bits << ETANKS);
     // we only keep the rightmost 20 bits
     rightmost = rightmost & mask;
 
-     uint32_t result = leftmost | rightmost;
+    uint32_t result = leftmost | rightmost;
     return result;
 }
 
  uint32_t generatePassword(struct options* config) {
 
-     uint32_t bits = 0x00;
-     const short ETANKS = config->etanks;
+    uint32_t bits = 0x00;
+    const uint8_t ETANKS = config->etanks;
 
     bits = bits | bubbleman( config->bubbleman );
     bits = bits | airman( config->airman );
